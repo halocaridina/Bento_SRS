8cd94f4f0c017cc713e949730cac9c38e2add69c
Scott R. Santos <halocaridina@gmail.com>
2022-12-24 20:51:52 -0500

    Another test

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'Go to Sleep!',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: 'e041dbf51aa943ecae690cedf4875586', // Write here your API Key
